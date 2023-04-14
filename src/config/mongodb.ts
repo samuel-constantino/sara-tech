32a3bbc15203e4cb4c36d16a309eb443b2473e42
Samuel Constantino <samuel.junior@sanp.com.br>
2023-04-14 20:16:12 -0300

    debug

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,24 +1,22 @@
 import { MongoClient } from 'mongodb'
 
-// let uri = process.env.MONGODB_URI || "" // trick ts :(
-// let dbName = process.env.MONGODB_DB
 let uri = "mongodb+srv://sara-tech:XgpLwS5av3zSTr6f@sara.bei34zm.mongodb.net/?retryWrites=true&w=majority"
 let dbName = "client1"
 
 let cachedClient: any = null
 let cachedDb: any = null
 
-// if (!uri) {
-//   throw new Error(
-//     'Please define the MONGODB_URI environment variable inside .env'
-//   )
-// }
+if (!uri) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env'
+  )
+}
 
-// if (!dbName) {
-//   throw new Error(
-//     'Please define the MONGODB_DB environment variable inside .env'
-//   )
-// }
+if (!dbName) {
+  throw new Error(
+    'Please define the MONGODB_DB environment variable inside .env'
+  )
+}
 
 export async function connectToDatabase() {
   if (cachedClient && cachedDb) {
