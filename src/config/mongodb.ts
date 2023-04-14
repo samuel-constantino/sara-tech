e01478a24325eda5e87ef1ced2425a5705333fa6
Samuel Constantino <samuel.junior@sanp.com.br>
2023-04-14 20:40:22 -0300

    debug

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -6,29 +6,22 @@ let dbName = "client1"
 let cachedClient: any = null
 let cachedDb: any = null
 
-if (!uri) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env'
-  )
-}
-
-if (!dbName) {
-  throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env'
-  )
-}
-
 export async function connectToDatabase() {
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb }
   }
 
-  const client = await MongoClient.connect(uri)
+  try {
+    const client = await MongoClient.connect(uri)
+  
+    const db = await client.db(dbName)
+  
+    cachedClient = client
+    cachedDb = db
+  
+    return { client, db }
+  } catch(e: any) {
+    return {error: true, message: e.message}
+  }
 
-  const db = await client.db(dbName)
-
-  cachedClient = client
-  cachedDb = db
-
-  return { client, db }
 }
