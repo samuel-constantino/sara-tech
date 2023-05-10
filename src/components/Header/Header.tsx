8810b8fce2be9ce3892c90bab22f869a749b4618
Samuel Constantino <samuel.junior@sanp.com.br>
2023-05-09 21:34:24 -0300

    refact(header): add new header template. #time 2h

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from "next/link";
+import { useState } from 'react';
 
 interface link {
     label: String,
@@ -7,25 +8,76 @@ interface link {
 }
 
 const links: link[] = [
-    {label: "SARA Tech", href: "/"},
     {label: "Irrigação", href: "/irrigation"},
     {label: "Ambiente", href: "/environment"},
 ];
 
 const Header = () => {
-
+    const [navbar, setNavbar] = useState(false);
     return (
-        <>
-            <div className="relative bg-black text-white h-24">
-                <div className="container mx-auto max-w-content overflow-hidden flex items-center justify-between px-4 smd:px-6 xl:px-6 xl2xl:px-4 py-4 md:space-x-10">
-                    {links.map((link, index) => (
-                        <Link key={index} href={""+link.href} className="text-18px font-medium">
-                            {link.label}
-                        </Link>
-                    ))}
+        <nav className="w-full bg-gray-800 shadow">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <div className="md:hidden">
+                            <button
+                            className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                            onClick={() => setNavbar(!navbar)}
+                            >
+                            {navbar ? (
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-6 h-6 text-white"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                />
+                                </svg>
+                            ) : (
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-6 h-6 text-white"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                                </svg>
+                            )}
+                            </button>
+                        </div>
+                        <a href="#">
+                            <h2 className="text-2xl text-white font-bold">SARA TECH</h2>
+                        </a>
+                        <div>
+                            <h1 className='text-white'>Olá Cliente</h1>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden'}`}>
+                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            {links.map((link, index) => (
+                                <li className="text-white">
+                                    <Link key={index} href={""+link.href} className="text-18px font-medium">
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </>
+        </nav>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,22 @@
-import Head from 'next/head';
 import Header from '@/components/Header/Header';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Head>
         <title>Sara Tech</title>
         <meta name="description" content="Sistemas de Automação Residencial e Agrícola" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header />
       <main className='mx-4'>
         <div className='flex flex-col items-center justify-center mt-4'>
           <h1 className='text-3xl font-bold underline mb-4'>SARA TECH</h1>
           <p>Sistemas de Automação Residencial e Agrícola</p>
         </div>
       </main>
-    </>
-  )
+    </div>
+  );
 }
